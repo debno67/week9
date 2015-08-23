bd40705a7e5c74eccd2d90787985cf037e97fc5f
debno67 <debno67@cox.net>
2015-08-23 09:54:43 -0400

    carEx2

---
+++ carExercise2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary3;
+
+namespace CarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars myCars = new Cars();
+            myCars.Drive();
+            myCars.Stop();
+            myCars.Appearance();
+        }
+    }
+}
+
